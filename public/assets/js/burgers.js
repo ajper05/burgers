898af9c21824b066183fb63aa46ce4de2ce30ff5
Anthony <pokepics@hotmail.com>
2020-07-20 21:46:29 -0700

    currently functioning, needs to be set up to deploy to heroku also some minor bug fixes

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,39 @@
+$(function() {
+  $(".devour").on("click", function(event) {
+    var id = $(this).data("id");
+
+    var eaten = {
+      devoured: true
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: eaten
+    }).then(
+      function() {
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurger = {
+      name: $("#burg").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(
+      function() {
+        console.log("New burger added!");
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
+});
